3bb462cd61c86f5cee2f8559eb7d2bd16fec1206
Thomas Hsu <tomdenotela@gmail.com>
2016-02-27 13:21:04 -0800

    Leaderboard data storage is a go

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, abort
 import time
+from datetime import datetime 
 import postmates as pm
 
 app = Flask(__name__)
@@ -8,19 +9,40 @@ class logic:
 	def __init__(self):
 		self.state = {'data':'No data so far. '}
 		self.map = {}
-		self.ldr = {}
+		self.brd = {}
 
 	def update(self, json):
+		# Calculates time difference. 
+		def timeDiff(a, b):
+			f = lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ')
+			return str(f(b) - f(a))
+
 		self.state = json
 		if 'status' in json and json['status'] == 'pickup_complete':
+			# Add start time. 
 			self.map[json['delivery_id']] = {'start_time': json['created'], 'end_time': '', 'courier': json['data']['courier']}
 		elif 'status' in json and json['status'] == 'delivered':
 			if json['delivery_id'] in self.map and self.map[json['delivery_id']]['end_time'] == '':
+				# Add finish timestamp. 
 				self.map[json['delivery_id']]['end_time'] = json['created']
-			# do nothing
+				# Calculate Time Difference. 
+				self.map[json['delivery_id']]['time'] = timeDiff(self.map[json['delivery_id']]['start_time'], 
+					self.map[json['delivery_id']]['end_time'])
+				# Add to leaderboard. 
+				self.add(self.map[json['delivery_id']])
+				# Remove entry from live list. 
+				del self.map[json['delivery_id']]
 	
+	# Adds an entry to the leader board. Saves multiple runs by rider. 
+	def add(self, entry):
+		name = entry['courier']['name']
+		# Add name if it's not in the leaderboard. 
+		if not name in self.brd:
+			self.brd[name] = []
+		self.brd[name].append(entry)
+
 	def json(self):
-		return self.map
+		return {'map': self.map, 'brd': self.brd}
 
 
 state = logic()
