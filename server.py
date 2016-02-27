2259c01c35516057d63bf16c32ee7c3c969afa77
Thomas Hsu <tomdenotela@gmail.com>
2016-02-27 09:48:37 -0800

    SOme run keeping functionality.

--- server.py
+++ server.py
@@ -5,14 +5,23 @@ import postmates as pm
 app = Flask(__name__)
 
 class logic:
-        def __init__(self):
-                self.state = {'data':'No data so far. '}
+	def __init__(self):
+		self.state = {'data':'No data so far. '}
+		self.map = {}
+		self.ldr = {}
 
-        def update(self, json):
-                self.state = json
-
-        def json(self):
-                return self.state
+	def update(self, json):
+		self.state = json
+		if 'status' in json and json['status'] == 'pickup_complete':
+			self.map[json['delivery_id']] = {'start_time': json['created'], 'end_time': '', 'courier': json['data']['courier']}
+		elif 'status' in json and json['status'] == 'delivered':
+			if json['delivery_id'] in self.map and self.map[json['delivery_id']['end_time'] == '':
+				self.map[json['delivery_id']['end_time'] = json['created']]
+		else:
+			# do nothing
+	
+	def json(self):
+		return self.map
 
 
 state = logic()
@@ -27,11 +36,11 @@ def index():
 
 @app.route('/webhooks', methods = ['POST'])
 def webhooks():
-        if not request.json:
-                abort(500)
-        else:
-                state.update(request.json)
-        return jsonify({'value': 'success'}), 200
+	if not request.json:
+		abort(500)
+	else:
+		state.update(request.json)
+		return jsonify({'value': 'success'}), 200
 
 @app.route('/newest')
 def newest():
@@ -45,5 +54,5 @@ def after_request(response):
   return response
 
 if __name__ == '__main__':
-        app.debug = True
+	app.debug = True
 	app.run(host='0.0.0.0')
