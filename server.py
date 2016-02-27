01ee19f6f90a3b752e02d4c7e80b0c2b246f1f41
Thomas Hsu <tomdenotela@gmail.com>
2016-02-27 15:58:03 -0800

    texting proto and some cleaning.

--- server.py
+++ server.py
@@ -17,19 +17,45 @@ class logic:
 			f = lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ')
 			return f(b) - f(a)
 
+		def _cmp_delivery(d0, d1):
+			d0_time = timeDiff(d0['start_time'], d0['end_time'])
+			d1_time = timeDiff(d1['start_time'], d1['end_time'])
+			return 1 if d0_time.total_seconds() - d1_time.total_seconds() > 0 else -1
+
+		def _sub_delivery(d0, d1):
+			d0_time = timeDiff(d0['start_time'], d0['end_time'])
+			d1_time = timeDiff(d1['start_time'], d1['end_time'])
+			return d0_time.total_seconds() - d1_time.total_seconds()
+
 		self.state = json
 		if 'status' in json and json['status'] == 'pickup_complete':
 			# Add start time. 
-			self.map[json['delivery_id']] = {'start_time': json['created'], 'end_time': '', 'courier': json['data']['courier']}
+			self.map[json['delivery_id']] = {'start_time': json['created'], 'end_time': '', 'courier': json['data']['courier'], 'id':json['delivery_id']}
 		elif 'status' in json and json['status'] == 'delivered':
 			if json['delivery_id'] in self.map and self.map[json['delivery_id']]['end_time'] == '':
+				# easier reference. 
+				me = self.map[json['delivery_id']]
 				# Add finish timestamp. 
-				self.map[json['delivery_id']]['end_time'] = json['created']
+				me['end_time'] = json['created']
 				# Calculate Time Difference. 
-				self.map[json['delivery_id']]['time'] = str(timeDiff(self.map[json['delivery_id']]['start_time'], 
-					self.map[json['delivery_id']]['end_time']))
+				me['time'] = str(timeDiff(me['start_time'], 
+					me['end_time']))
 				# Add to leaderboard. 
-				self.add(self.map[json['delivery_id']])
+				self.add(me)
+
+				msg = me['courier']['name'] + ', '
+
+				if me['id'] == json['delivery_id']:
+					current_leader = self.current_leader()
+					if current_leader['name'] == me['courier']['name']:
+						msg = msg + "you are the new leader! You set a record of " + current_leader['best_effort']['time'] + '.\n'
+					else:
+						msg = "you set a personal record! You are " + _sub_delivery(current_leader['best_effort'], self.brd[json['courier']['name']]['best_effort']['id']) + " behind the current leader.\n"
+				else:
+					msg = "you are " + _sub_delivery(self.brd[json['courier']['name']]['best_effort'], self.map[json['delivery_id']]) + " behind your personal record. "
+					# You set a new PR! You are x off from the leader. 
+					# You are X off from your personal record. 
+				print msg
 				# TO DO: Twilio integration. 
 				# Contant the driver with some info. 
 				# Remove entry from live list. 
@@ -75,6 +101,9 @@ class logic:
 		for i in range (0, len(temp)):
 			retVal.append({i + 1: sorted(temp, key = lambda e:  e['best_effort'], cmp = _cmp_delivery)[i]})
 		return {'leaderboard':retVal}
+
+	def current_leader(self):
+		return self.ldrbrd()['leaderboard'][0]
 			
 
 	def json(self):
