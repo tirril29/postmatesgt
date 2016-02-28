6d9288b08dd29f07c4a7a615f03618d6f74f34fb
Aleks Kivuls <akivuls@yahoo.com>
2016-02-28 01:06:08 -0800

    Broken courseMap

---
+++ templates/courseMap.js
@@ -0,0 +1,42 @@
+loadDoc("http://ec2-50-112-190-90.us-west-2.compute.amazonaws.com:5000/getAll");
+
+function click(name) {
+    document.getElementById("name").innerHTML = name.toString();
+}
+
+function loadDoc(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     var arr = xhttp.response;
+     var json = JSON.parse(arr.toString());
+
+        for(var i = 0; i < json.users.length; i++){
+	    var row = "<a href='#' onclick=\"click("+json.users[i]+");\">" + json.users[i] +"</a>";
+           // var row = "<a class=\"list\" href='#'>" + json.users[i] + "</a>";
+	    document.getElementById("myDropdown").innerHTML += row;
+	 }
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
---
+++ templates/leaflet.js
@@ -0,0 +1,15 @@
+var map = L.map('map').setView([33.99279, -118.24379], 11);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.streets'
+}).addTo(map);
+
+//var latlngs = {L.latLng}
+//var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+
+// zoom the map to the polyline
+//map.fitBounds(polyline.getBounds());
